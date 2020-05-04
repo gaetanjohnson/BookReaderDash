b2c662eaf7d2871222d78ebb09bceb241c00916e
gaetanjohnson <gaewjohnson@gmail.com>
2020-05-04 16:07:17 +0200

    added microsecond selector, refactor folder
    concerning microsecond support: nanosEpoch feature seem to depict microseconds, but time feature is precise up to the nanosecond . Decided to have a precision up to ms for simplicity .

--- BookReaderDashApp/app.py
+++ BookReaderDashApp/app.py
@@ -5,121 +5,30 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 import dash
-import dash_table
-import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from utils import import_and_format
+from app_layout import generate_app_layout
+from utils import import_and_format, generate_slider
 
-columns_to_display = ['bidSz', 'bidPx', 'askPx', 'askSz', 'tradePx', 'tradeSz']
-names_to_display = ['Volume (Bid)', 'Bid', 'Ask', 'Volume (Ask)', 'Trade Price', 'Trade Volume']
+columns_to_display = ['time', 'date', 'bidSz', 'bidPx', 'askPx', 'askSz', 'tradePx', 'tradeSz']
+# names_to_display = ['Time', 'Volume (Bid)', 'Bid', 'Ask', 'Volume (Ask)', 'Trade Price', 'Trade Volume']
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 df = import_and_format('./data_book_big.csv')
 
 df['time_readable'] = df['nanosEpoch'] - 1565157926599450000
-
 fig = px.line(df, x="time_readable", y="bidPx", title="Bid")
 fig.update_xaxes(rangeslider_visible=True)
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    dcc.Graph(figure=fig),
-    html.Div(
-        html.H1(children='Book Reader'),
-        style={'display': 'flex', 'margin': 0, 'position': 'fixed', 'right': 0, 'top': 0, 'left': 0,
-               'padding': 0, 'font-size': 0, 'background-color': '#f6f6f6', 'border-bottom': '1px solid #cccccc',
-               'text-align': 'center'}
-    ),
-    html.Div([
-        html.Div(
-            dcc.DatePickerSingle(
-                id='date_picker',
-                min_date_allowed=dt(2010, 8, 5),
-                max_date_allowed=dt.today(),
-                initial_visible_month=dt(2019, 8, 7),
-                date=str(dt(2019, 8, 7))
-            ),
-            style={'width': '30%', 'float': 'left', 'display': 'inline-block'}
-        ),
-        html.Div(
-            children=[
-                html.Div([
-                    dcc.Checklist(id='all_hour',
-                                  options=[{'label': 'All', 'value': 'all_hour'}]),
-                    dcc.RangeSlider(
-                        id='hour_slider',
-                        min=0,
-                        max=24,
-                        step=1,
-                        value=[6, 10],
-                        marks={i: f'{i}h' for i in range(0, 24, 3)}
-                    )],
-                ),
-                html.Div([
-                    dcc.Checklist(id='all_min',
-                                  options=[{'label': 'All', 'value': 'all_min'}]),
-                    dcc.RangeSlider(
-                        id='minute_slider',
-                        min=0,
-                        max=60,
-                        step=0.5,
-                        value=[4, 10],
-                        marks={i: f'{i}min' for i in range(0, 60, 10)}
-                    )],
-                ),
-                html.Div([
-                    dcc.Checklist(id='all_second',
-                                  options=[{'label': 'All', 'value': 'all_second'}, ]),
-                    dcc.RangeSlider(
-                        id='second_slider',
-                        min=0,
-                        max=60,
-                        step=0.5,
-                        value=[5, 30],
-                        marks={i: f'{i}s' for i in range(0, 60, 10)}
-                    )],
-                )
-                ],
-            style={'width': '60%',  # 'float': 'right',
-                   'display': 'flex',
-                   'flex-direction': 'column'
-                   }
-        )],
-        style={'justify-content': 'space-between', 'display': 'flex', 'margin-top': '100px'}
-    ),
-
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": ["Bid", "Volume"], "id": 'bidSz'},
-                 {"name": ["Bid", "Price"], "id": 'bidPx'},
-                 {"name": ["Ask", "Price"], "id": 'askPx'},
-                 {"name": ["Ask", "Volume"], "id": 'askSz'},
-                 {"name": ["Trade", "Price"], "id": 'tradePx'},
-                 {"name": ["Trade", "Volume"], "id": 'tradeSz'}],
-# {"name": i, "id": j} for (i,j) in zip(names_to_display, columns_to_display)],
-        style_data_conditional=[
-            {
-                'if': {'row_index': 'odd'},
-                'backgroundColor': 'rgb(248, 248, 248)'
-            }],
-        style_cell={
-                'textAlign': 'center'
-            },
-        style_header={
-            'backgroundColor': 'rgb(230, 230, 230)',
-            'fontWeight': 'bold'
-        },
-        merge_duplicate_headers=True
-    ),
-])
+app.layout = generate_app_layout(fig)
 
 
 @app.callback(Output('table', 'data'),
               [Input('hour_slider', 'value'), Input('minute_slider', 'value'),
-               Input('second_slider', 'value'), Input('date_picker', 'date')])
-def update_figure(hour_value, minute_value, second_value, date):
+               Input('second_slider', 'value'), Input('micros_slider', 'value'),
+               Input('date_picker', 'date')])
+def update_figure(hour_value, minute_value, second_value, micros_value, date):
     filtered_df = df.copy()
     if date is not None:
         date = dt.strptime(re.split('T| ', date)[0], '%Y-%m-%d')
@@ -134,8 +43,20 @@ def update_figure(hour_value, minute_value, second_value, date):
     if second_value is not None:
         min_second, max_second = second_value
         filtered_df = filtered_df[(filtered_df.second <= max_second) & (filtered_df.second >= min_second)]
+    if micros_value is not None:
+        min_micros, max_micros = micros_value
+        print(min_micros, max_micros)
+        filtered_df = filtered_df[(filtered_df.microsecond <= max_micros) & (filtered_df.microsecond >= min_micros)]
     df_to_display = filtered_df[columns_to_display].to_dict('records')
     return df_to_display
+
+@app.callback(
+    Output('output_timeframe', 'children'),
+    [Input('hour_slider', 'value'), Input('minute_slider', 'value'),
+     Input('second_slider', 'value'), Input('micros_slider', 'value'),
+     Input('date_picker', 'date')])
+def generate_output_timeframe(hour_value, minute_value, second_value, micros_value, date):
+    return f'Selected Timeframe: {date.split()[0]} {hour_value}h {minute_value}min {second_value}s {micros_value}ms'
 
 
 @app.callback(
@@ -158,6 +79,11 @@ def set_hour_values(value):
 def set_hour_values(value):
     return [0, 60] if value else [5, 30]
 
+@app.callback(
+    Output('micros_slider', 'value'),
+    [Input('all_micros', 'value')])
+def set_micros_values(value):
+    return [0, 1000000] if value else [597045, 650000]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
