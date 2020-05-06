75b200d1076c8d4a7b18ac6856b1d7e795672ee6
gaetanjohnson <gaewjohnson@gmail.com>
2020-05-06 15:40:39 +0200

    new data generator

--- BookReaderDashApp/data/data_generator.py
+++ BookReaderDashApp/data/data_generator.py
@@ -1,43 +1,95 @@
 import numpy as np
 import csv
 import pandas as pd
-
+from pathlib import Path
 path = 'data/data_book_big.csv'
 
 # Creates new set of data
 # TODO: Stop using fixed values in fonction
 
-def create_more_date(number):
-    volumes = np.round(np.random.binomial(200, 0.5, 2 * number), 1)
-    start_nanos_epoch = 1565157926599450000
-    periods = start_nanos_epoch + np.cumsum(np.random.binomial(1000, 0.5, number) * 1000)
-    datetimes = pd.to_datetime(periods)
-    msuk, source, tradePx, tradeSz, channelId, seqNum, msgIdx = 79889147, 'EUREX', 0, 0, 0, 597283, 0
-    prices_bid = create_prices(number)
-    prices_ask = prices_bid + np.round(np.random.gamma(0.2, 0.1, size=number), 2)
-    data = []
-    for i in range(number):
-        line = [periods[i], datetimes[i], msuk, source,
-                prices_bid[i], volumes[2 * i], prices_ask[i], volumes[2 * i + 1],
-                prices_bid[i], volumes[2 * i], prices_ask[i], volumes[2 * i + 1],
-                tradePx, tradeSz, channelId, seqNum, msgIdx]
-        data.append(line)
-    return data
+class DataGenerator:
+    FIRST_LINE = 'nanosEpoch,time,msuk,source,cbidPx,cbidSz,caskPx,caskSz,bidPx,bidSz,askPx,askSz,tradePx,tradeSz,channelId,seqNum,msgIdx'
+    DATA_DIR = Path(__file__).resolve().parent.joinpath("../data")
 
+    @classmethod
+    def _save_data(cls, mode, path, data):
+        with open(path, mode='w') as f:
+            writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for line in data:
+                writer.writerow(line) if mode == 'top' else f.write(line + '\n')
 
-def create_prices(number):
-    percent_changes = np.random.normal(0.0001, 0.01, number)
-    prices = 82.2 * np.exp(np.cumsum(percent_changes))
-    return np.round(prices, 3)
+    @classmethod
+    def _generate_data(cls, quantity, mode, **kwargs):
+        start_nanos_epoch = kwargs.get('start_nanos_epoch', 1565157926599450000)
+        average_time_delta = kwargs.get('average_time_delta', 1000)
+        msuk = kwargs.get('msuk', 79889147)
+        source = kwargs.get('source', 'EUREX')
+        channelId = kwargs.get('channelId', 0)
+        msgIdx = kwargs.get('msgIdx', 0)
+        average_volume = kwargs.get('average_volume', 200)
+        variance = kwargs.get('variance', 0.5)
+        base_rate = kwargs.get('base_rate', 0.001)
+        vol = kwargs.get('vol', 0.003)
+        base_price = kwargs.get('base_price', 82.2)
+        seqNum = kwargs.get('seqNum', 597283)
+        times = cls._generate_times(quantity, start_nanos_epoch, average_time_delta)
+        datetimes = pd.to_datetime(times)
+        volumes = cls._generate_volumes(quantity, average_volume, variance)
+        prices_bid = cls._generate_prices(quantity, base_rate, vol, base_price)
+        spreads = cls._generate_spreads(quantity)
+        prices_ask = prices_bid + spreads
+        trade_prices = cls._generate_trades(quantity)
+        volumes_bid, volumes_ask, volumes_trade = volumes[:quantity], volumes[quantity:2*quantity], volumes[:2*quantity]
+        data = []
+        for i in range(quantity):
+            if mode == 'top':
+                line = [times[i], datetimes[i], msuk, source,
+                        prices_bid[i], volumes_bid[i], prices_ask[i], volumes_ask[i],
+                        prices_bid[i], volumes_bid[i], prices_ask[i], volumes_ask[i],
+                        trade_prices[i], volumes_trade[i], channelId, seqNum, msgIdx]
+            else:
+                line = f'source Modify CAU19({msuk}) Sell {volumes_trade[i]}@{trade_prices[i]} (new qty/price) ' \
+                       f'lvl=6 new number of orders = 31 src={datetimes[i]} CDT our={datetimes[i]} CDT flags= ' \
+                       f'seq={seqNum} bid:{volumes_bid[i]}@{prices_bid[i]} ask:{volumes_ask[i]}@{prices_ask[i]} ' \
+                       f'cbid:{volumes_bid[i]}@{prices_bid[i]} cask:{volumes_ask[i]}@{prices_ask[i]}'
 
+            data.append(line)
+        return data
 
-def save_data(data, path):
-    with open(path, mode='w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for line in data:
-            writer.writerow(line)
+    @classmethod
+    def _generate_times(cls, quantity, start_nanos_epoch, average_time_delta):
+        return start_nanos_epoch + np.cumsum(np.random.binomial(average_time_delta, 0.5, quantity) * 1000)
 
+    @classmethod
+    def _generate_volumes(cls, quantity, average_volume, variance):
+        return np.round(np.random.binomial(average_volume, variance, 3 * quantity), 1)
 
-# f'source Modify CAU19(79889147) Sell 1819@82.8 (new qty/price) lvl=6 new number of orders = 31 src=2019-08-07 01:14:22.433700419 CDT our=2019-08-07 01:14:22.433706420 CDT flags= seq=227633875336 bid:37@82.2 ask:1137@82.3 cbid:37@82.2 cask:1137@82.3'
-# data = create_more_date(1000)
-# save_data(data)
+    @classmethod
+    def _generate_prices(cls, quantity, base_rate, vol, base_price):
+        percent_changes = np.random.normal(base_rate, vol, quantity)
+        prices = base_price * np.exp(np.cumsum(percent_changes))
+        return np.round(prices, 3)
+
+    @classmethod
+    def _generate_spreads(cls, quantity):
+        return np.round(np.random.gamma(0.2, 0.1, size=quantity), 2)
+
+    # TODO: Think of clever ways to generate trades
+    @classmethod
+    def _generate_trades(cls, quantity):
+        return [0] * quantity
+
+    @classmethod
+    def create_more_data(cls, quantity, mode='top', **kwargs):
+        if mode not in ['top', 'line']:
+            raise ValueError('Data Generation mode should be either top or line')
+        data = cls._generate_data(quantity, mode, **kwargs)
+        extension = '.csv' if mode == 'top' else '.txt'
+        file_name = 'new_data' + extension
+        path = cls.DATA_DIR / file_name
+        cls._save_data(mode, path, data)
+
+# quantity = 200
+# mode = 'top'
+# params = {}
+# DataGenerator.create_more_data(quantity, mode, **params)
