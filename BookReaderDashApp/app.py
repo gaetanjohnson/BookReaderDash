7b14b8dede1329a55d91f1b99ef930ba20e543dd
ahmed <alouaneahmedtaha@gmail.com>
2020-05-13 02:53:11 +0200

    fixed index problem

--- BookReaderDashApp/app.py
+++ BookReaderDashApp/app.py
@@ -92,7 +92,6 @@ def filter_dataframe(df, attr, range):
 def generate_depth_figure_non_cum(df, scale):
     df = pd.read_json(df, orient='split')
     fig = FigureGenerator.depth_non_cum_figure(df, scale)
-
     return fig
 
 @app.callback(
